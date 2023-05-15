fa1386b676bc7364f1d9c32af07d226896a1b7ff
ericchow8@gmail.com <ericchow8@gmail.com>
2023-05-15 17:02:37 +0200

    màj js & css

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+
+const poids = document.querySelector('#weight').value;
+const taille = document.querySelector('#height').value;
+
+
+
+const form = document.querySelector('.formulaire')
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+   
+})
+
+function displayResult(taille, poids){
+   
+    let mutiply = taille*2;
+    
+}
+
+
+console.log(mutiply);
