eb47482d4815f7cf2724357a0ef913af96269137
ericchow8@gmail.com <ericchow8@gmail.com>
2023-05-16 17:02:42 +0200

    update correction rachid

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,4 +18,44 @@ function displayResult(taille, poids){
 }
 
 
-console.log(mutiply);
+// console.log(mutiply);
+const forms = document.forms.imcForm
+console.log(forms.height.value);
+// *correction Rachid
+
+// const formImc = document.forms.imcForm;
+
+// formImc.addEventListener("submit", function (event) {
+//     event.preventDefault();
+
+//     const weight = formImc.weight.value;
+//     const size = formImc.size.value;
+const isGood = isValidForm(weight, size)
+if(isGood){
+    //     const result = calculateImc(weight,size)
+    //     createElement(result.toFixed(2))
+
+}
+
+    
+// });    
+
+// function calculateImc(weight, size){
+//   const result = weight / Math.pow(size/100, 2)
+//   return result
+// }
+
+// function createElement(imc){
+//     const messageResult = document.createElement('div')
+//     messageResult.textContent = `Votre IMC est de ${imc}`
+//     messageResult.classList.add('result')
+//     formImc.insertAdjacentElement('afterend', messageResult)
+//     formImc.weight.value = ""
+//     formImc.size.value = ""
+// }
+
+function isValidForm(weight, size){
+    let result = true
+
+    return result
+}
